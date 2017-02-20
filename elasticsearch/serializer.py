a5804476cdae9fdf65ea6f14ed233fc54c9fba76
Yash Mehrotra <yashmehrotra95@gmail.com>
2017-02-20 17:30:45 +0530

    Fix default kwarg

--- elasticsearch/serializer.py
+++ elasticsearch/serializer.py
@@ -42,7 +42,7 @@ class JSONSerializer(object):
             return data
 
         try:
-            return json.dumps(data, default=self.default, ensure_ascii=False)
+            return json.dumps(data, ensure_ascii=False)
         except (ValueError, TypeError) as e:
             raise SerializationError(data, e)
 
